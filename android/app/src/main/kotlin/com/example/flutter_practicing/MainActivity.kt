752b3475ed0404de248c5a1fcacb20af903245cf
Carlos Augusto <carlos@MacBook-Pro-de-Carlos.local>
2022-03-27 02:22:16 -0300

    first-commit

---
+++ android/app/src/main/kotlin/com/example/flutter_practicing/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_practicing
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primaryColor: Colors.green,
+        ),
+        home: HomePage());
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Meu Aplicativo',
+        ),
+      ),
+      body: Container(
+        color: Color.fromARGB(255, 92, 92, 92),
+        child: Center(
+          child: Text(
+            'Hello World!',
+            style: TextStyle(
+              fontSize: 30,
+              color: Colors.green,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
